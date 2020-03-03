bd05097c79a02d21378bcd7efa7c27949b4219ac
ddrake <ddrake@cselab5-041.cs.unl.edu>
2020-03-02 21:17:30 -0600

    Initial Commit

--- hw2/csce322a02part01.js
+++ hw2/csce322a02part01.js
@@ -1,14 +1,84 @@
 module.exports = {
-    onePlayerOneMove: onePlayerOneMove
+	onePlayerOneMove : onePlayerOneMove
 }
 
-var helpers = require( './helpers' );
+var helpers = require('./helpers');
 
-function onePlayerOneMove( game ){
-    
-    function whatever( move ){
-	return game;
-    }
+function onePlayerOneMove(game) {
 
-    return whatever;
+	function whatever(move) {
+
+		var playerPositionI = 0;
+		var playerPositionJ = 0;
+
+		// get the players current position
+		for (var i = 0; i < game.length; i++) {
+			for (var j = 0; j < game[0].length; j++) {
+				if (game[i][j] != '-' && game[i][j] != 'x') {
+					playerPositionI = i;
+					playerPositionJ = j;
+				}
+			}
+		}
+
+		// for every player an invalid move is:
+		// trying to move out of bounds
+		// trying to move into a '-'(empty space)
+		console.error(playerPositionI);
+		console.error(playerPositionJ);
+		console.error(game[0].length);
+
+		switch (move) {
+		case 'l':
+			if (playerPositionJ > 1) {
+				if (game[playerPositionI][playerPositionJ - 1] != '-'
+						&& game[playerPositionI][playerPositionJ - 2] != '-') {
+					game[playerPositionI][playerPositionJ] = 'x';
+					game[playerPositionI][playerPositionJ - 1] = '-';
+					game[playerPositionI][playerPositionJ - 2] = '1';
+
+				}
+			}
+			return game;
+			break;
+		case 'u':
+			if (playerPositionI > 1) {
+				if (game[playerPositionI - 1][playerPositionJ] != '-'
+						&& game[playerPositionI - 2][playerPositionJ] != '-') {
+					game[playerPositionI][playerPositionJ] = 'x';
+					game[playerPositionI - 1][playerPositionJ] = '-';
+					game[playerPositionI - 2][playerPositionJ] = '1';
+				}
+			}
+			return game;
+			break;
+		case 'r':
+			if (playerPositionJ < game.length - 1) {
+				if (game[playerPositionI][playerPositionJ + 1] != '-'
+						&& game[playerPositionI][playerPositionJ + 2] != '-') {
+					game[playerPositionI][playerPositionJ] = 'x';
+					game[playerPositionI][playerPositionJ + 1] = '-';
+					game[playerPositionI][playerPositionJ + 2] = '1';
+
+				}
+			}
+			return game;
+			break;
+		case 'd':
+			if (playerPositionI < game[0].length - 1) {
+				if (game[playerPositionI + 1][playerPositionJ] != '-'
+						&& game[playerPositionI + 2][playerPositionJ] != '-') {
+					game[playerPositionI][playerPositionJ] = 'x';
+					game[playerPositionI + 1][playerPositionJ] = '-';
+					game[playerPositionI + 2][playerPositionJ] = '1';
+				}
+			}
+			return game;
+			break;
+		}
+
+		return game;
+	}
+
+	return whatever;
 }
--- hw2/helpers.js
+++ hw2/helpers.js
@@ -1,69 +1,66 @@
 module.exports = {
-    readMovesFile:readMovesFile,
-    readGameFile:readGameFile,
-    printMoves:printMoves,
-    printGame:printGame
+	readMovesFile : readMovesFile,
+	readGameFile : readGameFile,
+	printMoves : printMoves,
+	printGame : printGame
 }
 
-function readMovesFile( file )
-{
-    var text;
-    var moves = [];
-    var rows = [];
-    var mvs = [];
-    var filesystem = require( 'fs' );
+function readMovesFile(file) {
+	var text;
+	var moves = [];
+	var rows = [];
+	var mvs = [];
+	var filesystem = require('fs');
 
-    text = filesystem.readFileSync( file );
-    rows = text.toString().split( "\n" );
-    rows.pop();
-    
-    moves = rows[0].split( ',' );
+	text = filesystem.readFileSync(file);
+	rows = text.toString().split("\n");
+	rows.pop();
 
-    for( var m = 0; m < moves.length; m++ ){
-	mvs[m] = moves[m];
-    }
-    
-    return mvs;
-}
+	moves = rows[0].split(',');
 
-function readGameFile( file )
-{
-    var text;
-    var game = [];
-    var rows = [];
-    var filesystem = require( 'fs' );
-
-    text = filesystem.readFileSync( file );
-    rows = text.toString().split( "\n" );
-    rows.pop();
-    
-    for( var r = 0; r < rows.length; r++ ){
-	game[r] = rows[r].split( ',' );
-    }
-
-    return game;
-}
-
-
-function printMoves( moves ){
-    console.log( moves );
-}
-
-function printGame( game ){
-    for( var r = 0; r < game.length; r++ ){
-	console.log( game[r] );
-    }
-}
-
-function findPlayers( game ){
-    var players = 0;
-    for( var r = 0; r < game.length; r++ ){
-	for( var c = 0; c < game[0].length; c++ ){
-	    if( game[r][c] != '-' && game[r][c] != 'x' && parseInt( game[r][c] ) > players ){
-		players = parseInt( game[r][c] );
-	    }
+	for (var m = 0; m < moves.length; m++) {
+		mvs[m] = moves[m];
 	}
-    }
-    return players;
+
+	return mvs;
 }
 
+function readGameFile(file) {
+	var text;
+	var game = [];
+	var rows = [];
+	var filesystem = require('fs');
+
+	text = filesystem.readFileSync(file);
+	rows = text.toString().split("\n");
+	rows.pop();
+
+	for (var r = 0; r < rows.length; r++) {
+		game[r] = rows[r].split(',');
+	}
+
+	return game;
+}
+
+function printMoves(moves) {
+	console.log(moves);
+}
+
+function printGame(game) {
+	for (var r = 0; r < game.length; r++) {
+		console.log(game[r]);
+	}
+}
+
+function findPlayers(game) {
+	var players = 0;
+	for (var r = 0; r < game.length; r++) {
+		for (var c = 0; c < game[0].length; c++) {
+			if (game[r][c] != '-' && game[r][c] != 'x'
+					&& parseInt(game[r][c]) > players) {
+				players = parseInt(game[r][c]);
+			}
+		}
+	}
+	return players;
+}
